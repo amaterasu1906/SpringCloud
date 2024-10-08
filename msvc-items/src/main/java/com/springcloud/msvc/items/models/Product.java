de474ac0bc39df63827ba23c5d7402752cb79bc0
domingo.mendoza <mg.domingoant@gmail.com>
2024-10-07 21:46:50 -0600

    Update Lombok en objetos

--- msvc-items/src/main/java/com/springcloud/msvc/items/models/Product.java
+++ msvc-items/src/main/java/com/springcloud/msvc/items/models/Product.java
@@ -2,15 +2,51 @@ package com.springcloud.msvc.items.models;
 
 import java.time.LocalDate;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class Product {
     private Long id;
     private String name;
     private Double price;
     private LocalDate createAt;
     private Integer port;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public LocalDate getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(LocalDate createAt) {
+        this.createAt = createAt;
+    }
+
+    public Integer getPort() {
+        return port;
+    }
+
+    public void setPort(Integer port) {
+        this.port = port;
+    }
+
 }
