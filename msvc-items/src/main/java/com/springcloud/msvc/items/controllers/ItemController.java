fd851ee6d97b92ef6cb7cf3aea523170146d1b68
domingo.mendoza <mg.domingoant@gmail.com>
2024-10-08 21:33:54 -0600

    Update Configuration RefreshScope, update sin refresh con Actuator

--- msvc-items/src/main/java/com/springcloud/msvc/items/controllers/ItemController.java
+++ msvc-items/src/main/java/com/springcloud/msvc/items/controllers/ItemController.java
@@ -2,13 +2,19 @@ package com.springcloud.msvc.items.controllers;
 
 import java.time.LocalDate;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,12 +28,33 @@ import com.springcloud.msvc.items.services.ItemService;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
-
+@RefreshScope
 @RestController
 public class ItemController {
     private final Logger logger = LoggerFactory.getLogger(ItemController.class);
     private final ItemService service;
     private final CircuitBreakerFactory cBreakerFactory;
+
+    @Value("${configuracion.texto}")
+    private String text;
+
+    @Autowired
+    private Environment env;
+
+    @GetMapping("/fetch-configs")
+    public ResponseEntity<Object> fetchConfig(@Value("${server.port}") String port) {
+        Map<String, String> json = new HashMap<>();
+        json.put("text", text);
+        json.put("port", port);
+        logger.info(text);
+        logger.info(port);
+        if( env.getActiveProfiles().length > 0 && env.getActiveProfiles()[0].equalsIgnoreCase("dev")){
+            json.put("autor.nombre", env.getProperty("configuracion.autor.nombre"));
+            json.put("autor.email", env.getProperty("configuracion.autor.email"));
+        }
+        return ResponseEntity.ok(json);
+    }
+    
 
     public ItemController(ItemService service, CircuitBreakerFactory cBreakerFactory) {
         this.service = service;
